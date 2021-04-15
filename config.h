9183e064c4a8410748c9747c4bc583378db76599
fracho123 <55332372+fracho123@users.noreply.github.com>
2021-04-15 19:46:03 -0400

    Update config.h

--- config.h
+++ config.h
@@ -1,6 +1,4 @@
 /*
-Copyright 2021 Franklin Choi <franklin.duksoo@gmail.com>
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
